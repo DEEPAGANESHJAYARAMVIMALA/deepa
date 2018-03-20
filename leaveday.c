313ff0b24cf860b617bb66af6d9dc28fe0b8c3e8
DEEPAGANESHJAYARAMVIMALA <30828753+DEEPAGANESHJAYARAMVIMALA@users.noreply.github.com>
2018-03-20 16:33:26 +0530

    Create leaveday.c

---
+++ leaveday.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  char a[50];
+  printf("enter the string");
+  scanf("%s",a);
+  if(a[0]=='a'||a[0]=='A')
+  {
+  
+    printf("yes");}
+    else
+    {
+      printf("no");
+    }
+    return 0;
+}
